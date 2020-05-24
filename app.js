597f3765491a06b02ea05fd64acd12fce6cd1ce5
Pranav <50168375+vapranav@users.noreply.github.com>
2020-05-24 15:36:22 +0530

    Add files via upload
    Initial application

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+
+app.get("/", function(req, res){
+	res.render("landing");
+});
+
+app.get("/form", function(req, res){
+	res.render("form");
+})
+
+app.get("/beds", function(req, res){
+	res.render("beds");
+})
+
+// app.get("/results", function(req, res){
+// 	var query = req.query.search;
+// 	var url = "http://www.omdbapi.com/?s=" + query +"&apikey=thewdb";
+// 	request(url, function(error, response, body){
+// 		if(!error && response.statusCode == 200){
+// 			var data = JSON.parse(body);
+// 			console.log(data);
+// 			res.render("results", {data: data});
+// 		}
+// 	});
+// });
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server Has Started!");
+});
